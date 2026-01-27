85964b1cfc476867890fce0196046a330c4cab15
nialov <nikolasovaskainen@gmail.com>
2026-01-27 22:02:15 +0200

    feat: add per-system nix module with pre-commit hooks

---
+++ nix/modules/per-system.nix
@@ -0,0 +1,72 @@
+(
+  { inputs, ... }:
+
+  {
+    perSystem =
+      {
+        config,
+        system,
+        pkgs,
+        lib,
+        ...
+      }:
+      let
+        mkNixpkgs =
+          nixpkgs:
+          import nixpkgs {
+            inherit system;
+            overlays =
+
+              [
+                inputs.nix-extra.overlays.default
+              ];
+            config = {
+              allowUnfree = true;
+            };
+          };
+
+      in
+      {
+        _module.args.pkgs = mkNixpkgs inputs.nixpkgs;
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = lib.attrValues { inherit (pkgs) python-env; };
+            shellHook = config.pre-commit.installationScript;
+          };
+
+        };
+
+        pre-commit = {
+          check.enable = true;
+          settings.hooks = {
+            nixfmt-rfc-style.enable = true;
+            nbstripout.enable = true;
+            shellcheck.enable = true;
+            statix.enable = true;
+            deadnix.enable = true;
+            rstcheck.enable = true;
+            yamllint = {
+              enable = true;
+            };
+            commitizen.enable = true;
+            ruff = {
+              enable = true;
+            };
+          };
+
+        };
+        legacyPackages = pkgs;
+
+        packages = {
+            update-blocklist = pkgs.writeShellApplication {
+                name = "update-blocklist";
+                text = ''
+                    ${pkgs.wget}/bin/wget https://download.dnscrypt.info/blacklists/domains/mybase.txt --output-document blocklists/mybase.txt
+                    # Check that mybase.txt is not empty AI!
+                '';
+            };
+        };
+      };
+
+  }
+)
