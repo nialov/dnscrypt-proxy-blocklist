84345a54bbf5fb6f79ad15b4be73d928360504a1
nialov <nikolasovaskainen@gmail.com>
2026-01-27 22:10:26 +0200

    chore: Add custom-pre-commit-hooks to flake.nix

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
         {
           systems = [ "x86_64-linux" ];
           imports = [
+            inputs.nix-extra.flakeModules.custom-pre-commit-hooks
             (inputs.import-tree ./nix/modules)
           ];
         }
