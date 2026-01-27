83eae12e09d698310763b578e99ea387f4f6f5ba
nialov <nikolasovaskainen@gmail.com>
2026-01-27 22:04:34 +0200

    init project

---
+++ nix/modules/ci.nix
@@ -0,0 +1,70 @@
+{ self, lib, ... }:
+{
+
+  flake.actions-nix = {
+    pre-commit.enable = true;
+    defaults = {
+      jobs = {
+        timeout-minutes = 60;
+        runs-on = "ubuntu-latest";
+      };
+    };
+    workflows =
+
+      let
+        checkoutStep = {
+          uses = "actions/checkout@v5";
+        };
+        installNixStep = {
+          # uses = "DeterminateSystems/nix-installer-action@v9";
+          uses = "cachix/install-nix-action@v31";
+        };
+
+      in
+      {
+        ".github/workflows/main.yaml" = {
+          on = {
+            push = { };
+            workflow_dispatch = { };
+            # pull_request = { };
+          };
+
+          jobs = {
+            "nix-flake-check".steps = [
+              checkoutStep
+              installNixStep
+              {
+                name = "Run nix flake checks";
+                run = "nix flake check";
+              }
+            ];
+          };
+
+        };
+        ".github/workflows/update-blocklist.yaml" = {
+          on = {
+            push = { };
+            workflow_dispatch = { };
+            # pull_request = { };
+          };
+
+          jobs = {
+            "update-blocklist".steps = [
+              checkoutStep
+              installNixStep
+              {
+                name = "Make directory";
+                run = "mkdir -p blocklists/";
+              }
+              {
+                name = "Download latest";
+                run = "wget https://download.dnscrypt.info/blacklists/domains/mybase.txt --output-document blocklists/mybase.txt";
+              }
+            ];
+          };
+
+        };
+
+      };
+  };
+}
